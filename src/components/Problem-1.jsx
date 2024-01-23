a1f6f351e967395ff7aeb5d886a18ac58225266a
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-01-23 22:54:06 +0600

    Solve Problem 1

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,10 +1,80 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Problem1 = () => {
+  // add states
   const [show, setShow] = useState("all");
+  const [data, setData] = useState([]);
+  const [visibleData, setVisibleData] = useState([]);
+  const [cur, setCur] = useState(0);
 
+  const filterData = (data, status, setVisibleData) => {
+    // if status all then sort and return
+    if (status === "all") {
+      const tempVisibleData = data.sort((a, b) => {
+        return a.statusCode - b.statusCode;
+      });
+
+      setVisibleData(tempVisibleData);
+      return;
+    }
+
+    // if status is specific filter and return
+    const tempVisibleData = data.filter((el) => {
+      const tempStatus = el.status.toLowerCase();
+      return tempStatus === status;
+    });
+
+    setVisibleData(tempVisibleData);
+    return;
+  };
+
+  // detect changes to show and data
+  useEffect(() => {
+    if (data.length) {
+      filterData(data, show, setVisibleData);
+    }
+  }, [show, data.length]);
+
+  // handle tab click
   const handleClick = (val) => {
     setShow(val);
+  };
+
+  // handle form submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const name = form.name.value.trim();
+    const status = form.status.value.trim();
+
+    // if any of the inputs are missing terminate from here
+    if (!name || !status) {
+      return;
+    }
+
+    // assign number status code for sorting
+    const statusToCheck = status.toLowerCase();
+    const statusCode =
+      statusToCheck === "active" ? 1 : statusToCheck === "completed" ? 2 : 3;
+
+    // change data state array
+    setData((prevData) => {
+      const tempData = [...prevData];
+      tempData.push({
+        id: cur,
+        name,
+        status,
+        statusCode,
+      });
+      return tempData;
+    });
+
+    // change cur number
+    setCur((prev) => prev + 1);
+
+    // reset input fields
+    e.target.reset();
   };
 
   return (
@@ -12,17 +82,32 @@ const Problem1 = () => {
       <div className="row justify-content-center mt-5">
         <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
         <div className="col-6 ">
-          <form className="row gy-2 gx-3 align-items-center mb-4">
-            <div className="col-auto">
-              <input type="text" className="form-control" placeholder="Name" />
-            </div>
+          {/* form starts */}
+          <form
+            onSubmit={handleSubmit}
+            className="row gy-2 gx-3 align-items-center mb-4"
+          >
+            {/* name field */}
             <div className="col-auto">
               <input
+                name="name"
+                type="text"
+                className="form-control"
+                placeholder="Name"
+              />
+            </div>
+
+            {/* status field */}
+            <div className="col-auto">
+              <input
+                name="status"
                 type="text"
                 className="form-control"
                 placeholder="Status"
               />
             </div>
+
+            {/* submit button */}
             <div className="col-auto">
               <button type="submit" className="btn btn-primary">
                 Submit
@@ -30,6 +115,8 @@ const Problem1 = () => {
             </div>
           </form>
         </div>
+
+        {/* tabs */}
         <div className="col-8">
           <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
             <li className="nav-item">
@@ -60,6 +147,7 @@ const Problem1 = () => {
               </button>
             </li>
           </ul>
+
           <div className="tab-content"></div>
           <table className="table table-striped ">
             <thead>
@@ -68,7 +156,19 @@ const Problem1 = () => {
                 <th scope="col">Status</th>
               </tr>
             </thead>
-            <tbody></tbody>
+
+            {visibleData && visibleData.length > 0 && (
+              <tbody>
+                {visibleData.map((data) => {
+                  return (
+                    <tr key={data.id}>
+                      <td>{data.name}</td>
+                      <td>{data.status}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            )}
           </table>
         </div>
       </div>
